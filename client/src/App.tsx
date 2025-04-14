70cfbddbba86dbb5c7e8481dae23fd7e1e5ffac6
anitech-pro <info@anitech.pro>
2025-04-14 20:42:41 +0200

    fix: Add TooltipProvider to App component to resolve tooltip context error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import { WizardProvider } from './context/WizardContext';
+import { WizardLayout } from './components/WizardLayout';
+import { ProjectTypeStep } from './pages/ProjectTypeStep';
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import WizardPage from "@/pages/WizardPage";
 import FrontendSelection from "@/pages/FrontendSelection";
@@ -10,7 +15,7 @@ import InfraSelection from "@/pages/InfraSelection";
 import Summary from "@/pages/Summary";
 import ActionPlanPage from "@/pages/ActionPlanPage";
 import AppContainer from "@/components/AppContainer";
-import { WizardProvider } from "@/context/WizardContext";
+import { EnvironmentSetup } from "@/components/setup";
 import TestComponent from "./test-component";
 import AppTest from "./app-test";
 import IntegrationPage from "@/pages/IntegrationPage"; // Assuming this component exists
@@ -25,6 +30,7 @@ function Router() {
       <Route path="/infrastructure" component={InfraSelection} />
       <Route path="/summary" component={Summary} />
       <Route path="/action-plan" component={ActionPlanPage} />
+      <Route path="/environment-setup" component={EnvironmentSetup} />
       <Route path="/test" component={TestComponent} />
       <Route path="/app-test" component={AppTest} />
       <Route component={NotFound} />
@@ -32,17 +38,19 @@ function Router() {
   );
 }
 
-function App() {
+const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppContainer>
-        <WizardProvider>
-          <Router />
-        </WizardProvider>
-      </AppContainer>
-      <Toaster />
+      <TooltipProvider>
+        <AppContainer>
+          <WizardProvider>
+            <Router />
+          </WizardProvider>
+        </AppContainer>
+        <Toaster />
+      </TooltipProvider>
     </QueryClientProvider>
   );
-}
+};
 
 export default App;
